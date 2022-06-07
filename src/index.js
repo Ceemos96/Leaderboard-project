f3feb898e834e837345971d4b6176d4935a8630a
ceemos <olaide.suleiman96@gmail.com>
2022-06-07 13:55:09 +0100

    set up webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { camelCase } from 'lodash';
+import './styles.css';
+
+console.log(camelCase('webpack boilerplate project'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+      static: path.join(__dirname, 'dist')
+  }, 
+  mode: 'development'
+  
+
+};
