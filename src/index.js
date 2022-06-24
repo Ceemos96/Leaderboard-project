3ef74b0316066b3305487c8f1b8d361975de652f
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-06-23 18:47:52 -0700

    Add button functionality for user inputs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './styles.css';
 import './main.scss';
+import renderUsers from './modules/renderUsers.js';
+import addUser from './modules/addUser.js';
+
+const inputFields = document.getElementById('input-fields');
+const refreshButton = document.getElementById('refresh');
+
+window.onload = (e) => {
+  e.preventDefault();
+  renderUsers();
+};
+
+inputFields.onsubmit = (e) => {
+  e.preventDefault();
+  addUser();
+};
+
+refreshButton.onclick = (e) => {
+  e.preventDefault();
+  renderUsers();
+};
